fdf47d646d37d039e4394352025fe4c14f2ce031
Ram Indukuri <RIndukuri@rccl.com>
2019-03-10 15:31:58 -0500

    Working Example

--- src/test/scala/sample.scala
+++ src/test/scala/sample.scala
@@ -1,9 +1,8 @@
 
 import junit.framework.TestCase
 import org.junit.Assert._
-import com.knoldus.training.test21
 import org.junit.Test
-
+import com.knoldus.training.Sample
 import scala.collection.mutable.ListBuffer
 
 class ExampleSuite extends TestCase {
@@ -11,7 +10,7 @@ class ExampleSuite extends TestCase {
   var sb: StringBuilder = _
   var lb: ListBuffer[String] = _
 
-  override def setUp() {
+  override def setUp() :Unit = {
     sb = new StringBuilder("ScalaTest is ")
     lb = new ListBuffer[String]
     println("Setup")
@@ -24,7 +23,7 @@ class ExampleSuite extends TestCase {
 
   @Test
   def testtwo :Unit = {
-    assertEquals(test21.human401(1),"1I am a monkey")
+    assertEquals(Sample.human401(1),"1I am a monkey")
   }
 
   override def tearDown(): Unit = {
