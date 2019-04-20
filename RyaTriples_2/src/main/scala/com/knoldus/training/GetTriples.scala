8191a76d2fd1a5c7e0624e8509abf49a9ef8d6fe
joukoknoldus <jouko@knoldus.com>
2019-04-20 14:38:05 -0500

    Get triples from df
    Former-commit-id: b59b1a802b2df364ec1824ab18d4ffcaba848b36

--- RyaTriples_2/src/main/scala/com/knoldus/training/GetTriples.scala
+++ RyaTriples_2/src/main/scala/com/knoldus/training/GetTriples.scala
@@ -3,7 +3,7 @@ package com.knoldus.training
 import com.knoldus.common.{AppConfig, KLogger}
 import com.knoldus.spark.Transformers
 import org.apache.log4j.Logger
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession, functions}
 import org.apache.spark.{SparkConf, SparkContext}
 import java.util.Properties
 
@@ -14,7 +14,6 @@ import scala.annotation.tailrec
 import org.apache.log4j.Logger
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.SparkSession
 import org.eclipse.rdf4j.model._
 import org.eclipse.rdf4j.model.impl.{SimpleValueFactory, TreeModel}
 import org.eclipse.rdf4j.model.util.ModelBuilder
@@ -40,11 +39,19 @@ import org.apache.accumulo.core.client.TableExistsException
 import org.apache.accumulo.core.client.TableNotFoundException
 import org.apache.accumulo.core.client.mock.MockInstance
 import org.apache.accumulo.core.client.security.tokens.PasswordToken
+import org.apache.spark.sql.types.{DoubleType, IntegerType, LongType, StringType}
 
 //import org.apache.any23.rdf.RDFUtils
 
 import org.apache.any23.extractor.csv
 
+object RdfModel {
+  lazy val model=new impl.TreeModel()
+}
+
+object RdfValueFactory {
+  val vf=SimpleValueFactory.getInstance()
+}
 
 object GetTriples {
 
@@ -58,7 +65,7 @@ object GetTriples {
 
     val conf=new SparkConf().setMaster("local[2]").setAppName("RyaTriples")
     val sc=new SparkContext(conf)
-    /*
+
 
     // Spark Demo
     val spark = SparkSession
@@ -71,11 +78,40 @@ object GetTriples {
     AppConfig.setSparkSession(spark)
     import spark.implicits._
     import com.knoldus.spark.UDFs.containsTulipsUDF
-    */
-    val rddRows=sc.textFile("/home/jouko/dev/project/TrainingSprints/TrainingSprint8/RyaTriples/src/main/resources/Legacy_Purchase_Orders.csv")
-      .map( x => x.split(",") )
-    rddRows.collect.foreach( x => println(x.mkString ))
 
+    val path="/home/jouko/dev/project/TrainingSprints/TrainingSprint8/RyaTriples/src/main/resources/Legacy_Purchase_Orders.csv"
+
+    val df = spark.read
+      .format("csv")
+      .option("header", "true")
+      .option("inferSchema", true)
+      .load(path)
+
+    df.show(5, false)
+    val newColumns=df.columns.map( x => x.replace(" ", "_"))
+    val dfRenamed=df.toDF(newColumns: _*).withColumn("ROW_ID", functions.monotonically_increasing_id())
+    dfRenamed.columns.foreach(println)
+
+    val knoldus="http://www.knoldus.com/"
+
+    val columnNames=dfRenamed.columns
+
+    columnNames.foreach(println)
+
+    val vf=SimpleValueFactory.getInstance()
+
+    val columnIris=getColumnIris(knoldus, vf, columnNames)
+
+    RdfModel.model
+
+    dfRenamed.foreach( x => addToModel(x, columnIris) )
+
+
+
+    val rddRows=sc.textFile(path)
+      .map( x => x.split(",") )
+    //rddRows.collect.foreach( x => println(x.mkString ))
+    /*
     val rows=rddRows.collect
 
     val model=new impl.TreeModel()
@@ -92,29 +128,112 @@ object GetTriples {
 
     columnIris.foreach(println)
 
+    val convertTo=getConvertTo
+
     for {i <- 1 until rows.length} {
       val row = vf.createIRI(knoldus, "row_" + i)
       model.add(row, RDF.TYPE, vf.createLiteral("order"))
       for {j <- columnIris.indices} {
         if (rows(i)(j)!="") {
-          //if (isInt(j)) { model.add(row, columnIris(j), vf.createLiteral(rows(i)(j).toInt)) }
-          //else {  }
-          model.add(row, columnIris(j), vf.createLiteral(rows(i)(j)))
+          if (convertTo(j)=="Int") { model.add(row, columnIris(j), vf.createLiteral(rows(i)(j).toInt)) }
+          else if (convertTo(j)=="Double") { model.add(row, columnIris(j), vf.createLiteral(rows(i)(j).toDouble)) }
+          else { model.add(row, columnIris(j), vf.createLiteral(rows(i)(j))) }
+          //model.add(row, columnIris(j), vf.createLiteral(rows(i)(j)))
         }
       }
     }
 
+    */
     val outFile="test.ttl"
 
-    writeModel(outFile, model)
+    writeModel(outFile, RdfModel.model)
 
     //for { i <- 0 to rows.size } {
     //  val rowSubject=RDFUtils.iri(documentIRI.toString(), "row/" + i)
     //}
 
 
-    //spark.stop()
+    spark.stop()
 
+  }
+
+  def addToModel(row: Row, colNames: Array[IRI]): Unit = {
+
+    //val model=new impl.TreeModel()
+
+    val knoldus="http://www.knoldus.com/"
+
+    //val convertTo=getConvertTo
+    //println(row.schema.fields(0).dataType)
+
+    val vf=SimpleValueFactory.getInstance()
+    //println(row)
+
+      val rowIRI = vf.createIRI(knoldus, "row_" + row.getLong(row.size-1))
+      RdfModel.model.add(rowIRI, RDF.TYPE, vf.createLiteral("order"))
+      for {j <- 0 until row.size-1} {
+        if (row(j)!=null) {
+          //if (convertTo(j)=="Int") { model.add(row, columnIris(j), vf.createLiteral(rows(i)(j).toInt)) }
+          //else if (convertTo(j)=="Double") { model.add(row, columnIris(j), vf.createLiteral(rows(i)(j).toDouble)) }
+          //else { model.add(row, columnIris(j), vf.createLiteral(rows(i)(j))) }
+          //println(row.schema.fields(j).dataType)
+          row.schema.fields(j).dataType match {
+            //println(row.getString(j))
+            case StringType => RdfModel.model.add(rowIRI, colNames(j), vf.createLiteral(row.getString(j)))
+            case LongType => RdfModel.model.add(rowIRI, colNames(j), vf.createLiteral(row.getLong(j)))
+            case DoubleType => RdfModel.model.add(rowIRI, colNames(j), vf.createLiteral(row.getDouble(j)))
+            case IntegerType => RdfModel.model.add(rowIRI, colNames(j), vf.createLiteral(row.getInt(j)))
+          }
+        }
+      }
+  }
+
+  def getConvertTo: Array[String] = {
+    Array(
+     "String",  //0
+      "String", //1
+      "String", //2
+      "String", //3
+      "Double", //4
+      "Int",    //5
+      "String", //6
+      "Int",    //7
+      "String", //8
+      "String", //9
+      "String", //10
+      "String", //11
+      "String", //12
+      "String", //13
+      "String", //14
+      "Int",    //15
+      "String", //16
+      "Double", //17
+      "Int",    //18
+      "String", //19
+      "String", //20
+      "String", //21
+      "String", //22
+      "String", //23
+      "String", //24
+      //"Int",    //25
+      "String", //25
+      "String", //26
+      "String", //27
+      "String", //28
+      "String", //29
+      "String", //30
+      "String", //31
+      "Int",    //32
+      "Double", //33
+      "Int",    //34
+      "String", //35
+      "String", //36
+      "String", //37
+      "String", //38
+      "String", //39
+      "String", //40
+      "String" //41
+    )
   }
 
   def getColumnIrisRec(knoldus: String, vf: SimpleValueFactory, cols: Array[String], n: Int, result: Array[IRI]): Array[IRI] = {
