d744a7653bdcdf041dadf6dbdffde0954de5b7f7
joukoknoldus <jouko@knoldus.com>
2019-05-15 11:15:09 -0500

    Mesos and Marathon work without Spark
    Former-commit-id: d6fbf7fd7c0933e74b4db09fdbe49532612e88a0

--- MesosTest/StartMesosAgent.sh
+++ MesosTest/StartMesosAgent.sh
@@ -1,6 +1,8 @@
 #bin=$HOME/dev/software/mesos-1.8.0/build/bin/mesos-agent.sh
 bin=mesos-agent
 
+ip=`cat IpAddress.txt`
+
 #sudo $bin --master=192.168.1.5:5050 --work_dir=$HOME/mesos_work_dir \
 	#containerizers=mesos \
 	#--image_providers=docker
@@ -18,4 +20,4 @@ bin=mesos-agent
 #	--isolation=docker/runtime,filesystem/linux \
 #	--image_providers=docker
 
-sudo $bin --work_dir=/var/lib/mesos/agent  --master=zk://192.168.1.5:2181/mesos --containerizers=docker,mesos --executor_registration_timeout=5mins
+sudo -E env "PATH=$PATH" $bin --work_dir=/var/lib/mesos/agent  --master=zk://$ip:2181/mesos --containerizers=docker,mesos --executor_registration_timeout=5mins
